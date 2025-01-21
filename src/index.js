5f3dcaec3e55a2e199f7480be64b683c7ce61a86
ihorzhuk <ihorzhuk94@gmail.com>
2025-01-21 18:32:54 +0200

    grid component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { Clock } from './clock';
 import { Marquee } from './marquee';
+import { Grid } from './grid';
 
 import { reveal } from './helpers/reveal';
 
 const clock = new Clock('.hero_clock');
 const marqueeElements = document.querySelectorAll(".marquee");
 marqueeElements.forEach((marqueeElements) => new Marquee(marqueeElements));
+const grid = new Grid();
 
 reveal();
