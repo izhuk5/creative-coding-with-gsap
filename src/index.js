fb5c48160b22efcb4965b9428a60684a09f32a9c
ihorzhuk <ihorzhuk94@gmail.com>
2025-01-31 21:47:17 +0200

    toolkit text animation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Grid } from './grid';
 import { Heading } from './heading';
 
 import { reveal } from './helpers/reveal';
+import { ToolkitText } from './toolkit';
 
 const clock = new Clock('.hero_clock');
 const marqueeElements = document.querySelectorAll(".marquee");
@@ -13,5 +14,6 @@ const grid = new Grid();
 document.fonts.ready.then(() => {
   console.log('Fonts are loaded here');
   const heading = new Heading(document.querySelector('.hero_h1'));
-})
-reveal();
+  const toolkit = new ToolkitText('.toolkit_p');
+  reveal();
+});
